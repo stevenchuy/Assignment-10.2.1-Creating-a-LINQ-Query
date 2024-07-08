a4f5eb8f2fb832db2b2969806057fe5114425ad6
steve <steve@DESKTOP-8OHDCI7>
2024-07-08 14:02:59 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+
+namespace Assignment_10._2._1_Creating_a_LINQ_Query
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 2, -1, 3, -3, 10, -200 };
+
+            var numQuery =
+                from num in array
+                where num > 0
+                select num;
+
+            Console.Write("Positive numbers in query:");
+
+            foreach (int num in numQuery)
+            {
+                Console.Write( " " + num);
+            }
+        }
+    }
+
+}
